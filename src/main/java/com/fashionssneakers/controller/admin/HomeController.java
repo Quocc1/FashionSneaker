86bc51e8a371fd47d39062d6ec214eebc4ad179a
Quocc1 <caobinhoh@gmail.com>
2022-11-09 17:19:27 +0700

    Quốc: Thêm admin form

--- src/main/java/com/fashionssneakers/controller/admin/HomeController.java
+++ src/main/java/com/fashionssneakers/controller/admin/HomeController.java
@@ -1,18 +1,19 @@
 package com.fashionssneakers.controller.admin;
 
-import com.fashionsneakers.controller.web.*;
 import java.io.IOException;
-import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "HomeController", urlPatterns = {"/admin-home"})
+@WebServlet(name = "AdminHomeController", urlPatterns = {"/admin-home"})
 public class HomeController extends HttpServlet {
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        
+        RequestDispatcher rd = request.getRequestDispatcher("/views/admin/index.jsp");
+        rd.forward(request, response);
     }   
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
